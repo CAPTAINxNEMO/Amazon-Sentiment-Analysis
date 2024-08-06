25fa199478c02bb59f5a8fa07de11b15d13d25b5
CAPTAINxNEMO <shashank.v.bhave@gmail.com>
2024-08-06 20:14:17 +0530

    Rust learning - Functions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::io::{self, Write};
 
+fn addition(num1: f64, num2: f64) -> f64 {
+    let sum = num1 + num2;
+    return sum;
+}
+
 fn main() {
     print!("Enter num1: ");
     io::stdout().flush().unwrap();
@@ -13,6 +18,5 @@ fn main() {
     io::stdin().read_line(&mut num2).expect("Failed to read input");
     let num2: f64 = num2.trim().parse().expect("Please enter a valid number.");
 
-    let sum = num1 + num2;
-    println!("The sum of {} and {} is {}", num1, num2, sum);
+    println!("The sum of {} and {} is {}.", num1, num2, addition(num1, num2));
 }
