1ceca53974edbc00d64ceb07eeb6275f57b160b8
CAPTAINxNEMO <shashank.v.bhave@gmail.com>
2024-08-07 18:18:11 +0530

    fixed score printing

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use std::io::{self, Write};
 use std::error::Error;
 use std::fs::File;
 use std::path::Path;
+use csv::Reader;
 
 fn addition(num1: f64, num2: f64) -> f64 {
     let sum = num1 + num2;
@@ -10,17 +11,17 @@ fn addition(num1: f64, num2: f64) -> f64 {
 
 fn webpage_parser<P: AsRef<Path>>(filename: P) -> Result<(), Box<dyn Error>> {
     let file = File::open(filename)?;
-    let mut rdr = csv::Reader::from_reader(file);
+    let mut rdr = Reader::from_reader(file);
 
     for (i, result) in rdr.records().enumerate() {
         let record = result?;
         
         let review = &record[0];
-        // let score: f64 = record[1].parse()?;
+        let score: f64 = record[1].parse()?;
         let sentiment = &record[2];
 
-        // println!("Record {}:\nReview: {}\nScore: {}\nSentiment: {}\n", i + 1, review, score, sentiment);
-        println!("Record {}:\nReview: {}\nSentiment: {}\n", i + 1, review, sentiment);
+        println!("Record {}:\nReview: {}\nScore: {}\nSentiment: {}\n", i + 1, review, score, sentiment);
+        // println!("Record {}:\nReview: {}\nSentiment: {}\n", i + 1, review, sentiment);
     }
 
     Ok(())
